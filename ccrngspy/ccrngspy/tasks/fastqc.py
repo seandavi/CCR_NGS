dc3b7d9ffeed12d54c56942c6425db5256b8c741
Kenny Daily <kmdaily@gmail.com>
2012-02-15 16:25:11 -0500

    Changed optparse to argparse

--- ccrngspy/ccrngspy/tasks/fastqc.py
+++ ccrngspy/ccrngspy/tasks/fastqc.py
@@ -3,7 +3,7 @@
 Kenny Daily, 2012
 
 """
-import optparse
+import argparse
 import os
 
 from ccrngspy import utils
@@ -11,60 +11,61 @@ from ccrngspy import utils
 logger = utils.make_local_logger("FastQC logging", level="debug", color="green")
 
 class Task(object):
-    def optparse(self, parser):
+    def argparse(self, parser):
         """Add option group for the particular task to an OptionParser object.
         
         """
         
         raise NotImplementedError
-
+    
     def set_options(self, opts, args):
-        """Use opts and args from optparse.parse_args to set options for the class.
+        """Use opts and args from argparse.parse_args to set options for the class.
         
         """
-
+        
         raise NotImplementedError
-
-
+    
+    
 class FastQC(Task):
     """Container for FastQC tasks.
     
     """
-
+    
     _cmd = "fastqc"
-
+    
     _opt_lookup = dict(output_directory="-o %s",
                        threads="-t %s",
                        casava="--casava")
     
     def __init__(self, input_files=None, output_directory=None):
-        
         self.input_files = input_files
         self.output_directory = output_directory
 
-    def optparse(self, parser):
+    def argparse(self, parser):
         """Add FastQC option group to an OptionParser.
         
         """
 
-        group = optparse.OptionGroup(parser, "FastQC Options")
-        group.add_option("-o", "--outdir", dest="output_directory", type="string",
-                         help="FastQC output directory.")
-        group.add_option("--casava", dest="casava", action="store_true", default=False,
-                         help="Files come from raw casava output.")
-        group.add_option("-t", "--threads", dest="threads", type="int", default=1,
-                         help="Specifies the number of files which can be processed simultaneously.")
-        parser.add_option_group(group)
+        group = parser.add_argument_group("FastQC Options")
+        group.add_argument("-o", "--outdir", dest="output_directory", type=str,
+                           help="FastQC output directory.")
+        group.add_argument("--casava", dest="casava", action="store_true", default=False,
+                           help="Files come from raw casava output.")
+        group.add_argument("-t", "--threads", dest="threads", type=int, default=1,
+                           help="Specifies the number of files which can be processed simultaneously.")
+        group.add_argument("input_files", type=str, nargs="+",
+                           help="Input fastq files.")
+
         return parser
     
-    def set_options(self, opts, args):
-        """Use opts and args from optparse.parse_args to populate the class.
+    def set_options(self, args):
+        """Use opts and args from argparse.parse_args to populate the class.
 
         """
 
-        self.__init__(input_files=args, output_directory=opts.output_directory)
-        self.threads = opts.threads
-        self.casava = opts.casava
+        self.__init__(input_files=args.input_files, output_directory=args.output_directory)
+        self.threads = args.threads
+        self.casava = args.casava
 
     def make_option_string(self):
         """Use currently set options to create the option string.
@@ -102,13 +103,13 @@ def _test():
 
     fastqc = FastQC()
 
-    usage = "%prog [options] input_files"
-    parser = optparse.OptionParser(usage=usage)
-    parser = fastqc.optparse(parser)
+    usage = "%(prog)s [options] input_files"
+    parser = argparse.ArgumentParser(usage=usage)
+    parser = fastqc.argparse(parser)
     
-    (opts, args) = parser.parse_args()
+    args = parser.parse_args()
 
-    fastqc.set_options(opts, args)
+    fastqc.set_options(args)
     
     fastqc.run_fastqc()
     
