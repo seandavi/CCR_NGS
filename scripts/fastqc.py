43c509dc74a5793032aa3d7c2864c4e90bb61591
Kenny Daily <kmdaily@gmail.com>
2012-02-17 12:26:07 -0500

    Added docstrings, fixed inline documentation.

--- scripts/fastqc.py
+++ scripts/fastqc.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+"""Run the fastqc pipeline.
+
+This runs FastQC on a number of files.
+
+It requires a YAML configuration file with parameters for FastQC (output directory, etc.)
+It also requires a samples file that has at least a column named 'sample' and 'filename'.
+
+"""
 
 import csv
 import sys
@@ -24,23 +32,20 @@ parser.add_argument('--config_file', dest="config_file", type=str,
 parser.add_argument('--sample_file', dest="sample_file", type=str,
                     help="A YAML configuration file for pipeline.")
 
-## add options for the fastqc task
+# add options for the fastqc task
 parser = fastqc_task.argparse(parser)
 
-## Parse the options
+# Parse the options
 opts = parser.parse_args()
 
-## Load the bootstrap config file
+# Load the bootstrap config file
 with open(opts.config_file, 'r') as configfile:
     config = yaml.load(configfile)
 
+# Load the samples tab-separated file
 with open(opts.sample_file, 'r') as samplefile:
     reader = csv.DictReader(samplefile, delimiter="\t")
     samples = list(reader)
-
-#----------------------------------------------
-# begin tasks here
-#----------------------------------------------
 
 def make_fastqc_param_list(samples):
     """Helper function to turn the sample file into a list of files.
@@ -54,6 +59,10 @@ def make_fastqc_param_list(samples):
     return map(lambda x: [x['filename'], None, None], samples)
 
 test_task_params = make_fastqc_param_list(samples)
+
+#----------------------------------------------
+# begin tasks here
+#----------------------------------------------
 
 @files(test_task_params)
 def run_fastqc(input, output, params=None):
