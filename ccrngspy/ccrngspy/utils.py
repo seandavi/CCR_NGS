af049b4997fbdd337c5f245fb843d757bd834c8f
Kenneth Daily <dailykm@mail.nih.gov>
2012-03-19 14:41:52 -0400

    Added safe run for qsub commands, checking with a qstat/grep command if the job has finished

--- ccrngspy/ccrngspy/utils.py
+++ ccrngspy/ccrngspy/utils.py
@@ -1,6 +1,8 @@
 import subprocess
 import shlex
 import logging
+import tempfile
+import time
 
 def safe_run(cmd, shell=False):
     res = None
@@ -14,6 +16,70 @@ def safe_run(cmd, shell=False):
             res = subprocess.call(shlex.split(cmd))
     
     return(res)
+
+_script_header = """
+#!/bin/bash
+#PBS -N %(jobname)s
+#PBS -k oe
+"""
+
+def check_job(jobid):
+    """Check if a PBS job with 'jobid' is finished or not.
+
+    Does not currently implement any error checking, only if the
+    job is finished (it will not show up in qstat).
+
+    This only works with single jobs as well!
+    
+    """
+
+    # get the job from qstat -f, get the job state line, assume its the first one, cut out the status
+    cmd = 'qstat -f %(jobid)s | grep job_state | head -n 1 | sed "s/.*job_state = \(.*\)/\1/"' % dict(jobid=jobid)
+
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    status, stderr = proc.communicate()
+
+    status = status.rstrip()
+
+    # This is important - this will appear when the job no longer exists
+    # It could be that it crashed or otherwise failed!
+    test = "qstat: Unknown Job Id %s\n" % jobid
+    done = False
+
+    if stderr == test:
+        done = True
+
+    return status, done
+
+def safe_qsub_run(cmd, shell=False):
+    """Run a command via qsub.
+
+    Requires a header string, a job name.
+
+    Uses a hacky testing command using qstat to determine if the script is finished.
+    
+    """
+    
+    foo = tempfile.NamedTemporaryFile()
+    foo.write(_script_header % {'jobname': 'foobarbaz'})
+
+    foo.write("%s\n" % cmd)
+    foo.file.flush()
+
+    qsub_cmd = "qsub -l nodes=1 %s" % foo.name
+
+    print foo.name
+    proc = subprocess.Popen(qsub_cmd, shell=True, stdout=subprocess.PIPE)
+    jobid = proc.communicate()[0].rstrip()
+
+    done = False
+    while not done:
+        job_status, done = check_job(jobid)
+        time.sleep(5)
+
+    foo.close()
+
+    return proc
 
 _LOGGING_LEVEL = {'debug': logging.DEBUG,
                   'info': logging.INFO,
